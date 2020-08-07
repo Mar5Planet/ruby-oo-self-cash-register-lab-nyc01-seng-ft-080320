34ad3d717b807ffe954fb94c05fcc04e2a319502
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-07 18:06:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize(employee_discount = nil)
+    @total = 0
+    @discount = employee_discount
+  end
+  
+  def add_item(title, price)
+    @title = title 
+    @total += price
+  end 
+end
