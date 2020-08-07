aabb3adab39fc835a3940ccd3dd730fdf4e366f2
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-07 18:27:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :last_trans
   def initialize(employee_discount = nil)
     @total = 0
     @discount = employee_discount
