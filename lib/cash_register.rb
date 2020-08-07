5bb1fd9718ff19019204ce8ddb9634a88587c1db
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-07 18:13:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   
   def apply_discount 
     if @discount 
-      @total = 
+      @total -= (@discount/100) * @total
     else "There is no discount to apply."
     end 
   end 
